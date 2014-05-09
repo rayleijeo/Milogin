18c29d21ffef8709edd55d5e0ed6ab2b9aec6941
Ray's <zhangshuanglei@163.com>
2014-05-09 17:39:31 +0800

    del doc

--- milogin.py
+++ milogin.py
@@ -67,36 +67,3 @@ getcookie=pathinfo.sub('',getcookie)
 headers['Cookie']=getcookie
 content =gethtmlresult(yuyueurl,'GET',headers)
 print getcookie 
-'''
-Referer=yuyueurl 
-yuyueurl=urllib.unquote(yuyueurl[yuyueurl.find('=')+1:len(yuyueurl)]);
-headers['Cookie']=response['set-cookie']
-headers['Accept']='text/html, application/xhtml+xml, */*'
-headers['Host']=yuyueurl[8:yuyueurl.find('/',10)]
-ffff , fffccc =http.request(yuyueurl,'POST',headers=headers)
-print headers
-print fffccc.decode("utf-8")
-#登录成功后
-#print loginresult['desc'],utf8content
-''''''
-sucresponse , succontent =http.request(location,'GET',headers=headers)
-print sucresponse.status,sucresponse['set-cookie']
-allcookie=response['set-cookie']+sucresponse['set-cookie'] 
-getcookie=re.findall('passToken=([\s\S]*?); Dom',allcookie)
-print 'serviceToken='+getcookie[0]
-
-headers={'Cookie':'serviceToken='+getcookie[0],
-'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36',
-'Content-type':'application/x-www-form-urlencoded',
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-'Accept-Encoding':'gzip,deflate,sdch',
-'Accept-Language':'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'Host':'order.mi.com',
-'DNT':'1',
-'Referer':'http://a.hd.mi.com/product/waiting/a/27'} 
-
-newurl = 'http://a.hd.mi.com/product/check/a/27'
-ffff , fffccc =http.request(newurl,'POST',headers=headers)
-print ffff.status,ffff["location"]
-#!response, content = http.request(url, 'GET', headers=headers)'''
